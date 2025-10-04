70a0cf61566a7f4cc8d1a19e3fff3d7c56d9d896
Brenda Senhorinho <brendamirellisenhorinho@gmail.com>
2025-10-04 11:25:34 -0300

    att

---
+++ DataBase.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+    private $host = "localhost:3306";
+    private $db_name = "sistema_cbb";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexão realizada com sucesso!<br>";
+        } catch(PDOException $exception) {
+            echo "Erro de conexão: " . $exception->getMessage() . "<br>";
+        }
+        return $this->conn;
+    }
+}
+
